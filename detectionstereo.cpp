0026aab8fb1200f504669d60099530d8921d5af2
Pandakls <theophile.plancke@ensimag.imag.fr>
2014-12-17 20:56:55 +0100

    up to 3.3.4

--- detectionstereo.cpp
+++ detectionstereo.cpp
@@ -22,6 +22,7 @@ Mat calculDisp(Mat im1, Mat im2){
 
 //2.2 Computation of the 3D coordinates and remove all the pixels with a Z coodinate higher or lower than a threshold
 Mat compute3DAndRemove(Mat disp){
+    Mat res(disp.rows,disp.cols, CV_8U, Scalar(0));
     float thresMax = 2.5;
     float thresMin = 0.2;
     //float u0 = 258; //unused here
@@ -30,19 +31,20 @@ Mat compute3DAndRemove(Mat disp){
     float av = 410;
     float b = 0.22;
     float z0 = 1.28;
-    for(int u=0; u<disp.rows; u++){
-        unsigned char *input = disp.ptr<unsigned char>(u);
-        for(int v=0; v<disp.cols; v++) {
-            int d = input[v]/16;
+    for(int v=0; v<disp.rows; v++){
+        for(int u=0; u<disp.cols; u++) {
+            int d = disp.at<unsigned char>(v,u);
             //float x = (u-u0)*b/d - (b/2); //unused here
             //float y = au*b/d;             //unused here
-            float z = z0 - (((u-v0)*au*b)/(av*d));
+            float z = z0 - (((v-v0)*au*b)/(av*d/16));
             if(z<thresMin || z>thresMax){
-                input[v] = 0;
+                res.at<unsigned char>(v,u) = 0;
+            }else{
+                res.at<unsigned char>(v,u) = d;
             }
         }
     }
-    return disp;
+    return res;
 }
 
 //2.3.1 Compute the v-disparity image histogram of disparity matrix
@@ -52,7 +54,7 @@ Mat computeVDisparity(Mat img){
     for(int u=0; u<img.rows;u++){
         for(int v=0; v<img.cols; v++){
             int disp = (img.at<unsigned char>(u,v)) /8;
-            if(disp>0 && disp <= maxDisp){
+            if(disp>6 && disp < maxDisp-2){
                 vDisp.at<unsigned char>(u,disp) += 1;
             }
         }
@@ -63,7 +65,7 @@ Mat computeVDisparity(Mat img){
 //2.3.3 Removing noise from v disparity map
 Mat removeNoise(Mat img){
     Mat res = img.clone();
-    int thresh = 60;
+    int thresh = 30;
     //threshold( src_gray, dst, threshold_value, max_BINARY_value,threshold_type );
     threshold(img,res,thresh,255,3); // 255 > max binary value, 4 > zero inverted (to 0 under thresh)
     return res;
@@ -81,6 +83,26 @@ std::vector<Point2f> storeRemainingPoint(Mat img){
             }
         }
     }
+    return res;
+}
+
+
+ Mat filterRansac (Vec4f line, Mat img){
+     Mat res (img.rows, img.cols, CV_8U, Scalar(0));
+     double slope = line[0]/line[1];
+     double orig = line[2]-slope*line[3];
+     for(int u=0; u<img.rows;u++){
+         for(int v=0; v<img.cols; v++){
+            int value = img.at<unsigned char>(u,v);
+            double test = orig + slope*value/16 - u;
+            std::cout <<"test : "<< test << std::endl;
+            if(test < -30){
+                res.at<unsigned char>(u,v) = 0;
+            }else{
+                res.at<unsigned char>(u,v) = value;
+            }
+         }
+     }
     return res;
 }
 
@@ -112,16 +134,28 @@ int main(int argc, char **argv)
     Mat vDispNoisy = computeVDisparity(disp);
     imshow("VDisparity method", vDispNoisy);
 
+    //2.3.2 manually computation of ho & po :
+    /* We measure a line y = 3.36x +143,56
+     * where y is the heigh in pixel image and x the luminosity between 0 and 32 of disparity map
+     * The luminosity depends on the depth x=32 -> depth = 0 and x=0 -> depth = +infiny
+     * But this correlation is not linear and the y is not linked with y in real world
+     * so computing the height of road and it's slope seems difficult there..
+     */
+
     //2.3.3 Removing noise from v disparity map
     Mat vDisp = removeNoise(vDispNoisy);
     imshow("VDisparity map no noise", vDisp);
 
-    //2.3.4 extracting the remaining points and removing the floor
+    //2.3.3 extracting the remaining points and removing the floor
     std::vector<Point2f> tempVec = storeRemainingPoint(vDisp);
     //fitLineRansac(tempVec, line, iteration, sigma, a_max);
     Vec4f newline;
-    fitLineRansac(tempVec, newline, 15, 2, 1);
+    fitLineRansac(tempVec, newline);
     std::cout << newline << std::endl;
+
+    Mat finalDisp = filterRansac(newline, disp8);
+    imshow("Final Disparity filtered", finalDisp);
+
 
     // Display images and wait for a key press
     waitKey();
